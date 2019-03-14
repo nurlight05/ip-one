e51fbc2de96b3eb5c35aa47141780932a3585163
Max Lee <godmanshot@gmail.com>
2019-03-14 17:44:26 +0600

    1

--- app/Http/Controllers/ShowroomsController.php
+++ app/Http/Controllers/ShowroomsController.php
@@ -12,7 +12,24 @@ class ShowroomsController extends Controller
         $client = new Client();
         $res = $client->request('GET', 'http://88.99.171.85/showrooms/');
         $showrooms = json_decode((string)$res->getBody(), true);
+        $places = [];
+        $_country = $request->input('country', 'Россия');
+        $_city = $request->input('city', 'Москва');
 
-        return view('showrooms.index', compact('showrooms'));
+        foreach ($showrooms as $item) {
+            if(!isset($places[$item['COUNTRY']]) || !is_array($places[$item['COUNTRY']]))
+                $places[$item['COUNTRY']] = [];
+
+            if(!in_array($item['CITY'], $places[$item['COUNTRY']]))
+                $places[$item['COUNTRY']][] = $item['CITY'];
+        }
+        
+        $showrooms = array_filter($showrooms, function($item) use ($_city) {
+            if($item['CITY'] != $_city)
+                return false;
+            return true;
+        });
+
+        return view('showrooms.index', compact('showrooms', 'places', '_country', '_city'));
     }
 }
