33d7dea8b8ab125ce134b5975a4c889f9987d710
Max Lee <godmanshot@gmail.com>
2019-03-19 10:45:18 +0600

    1

--- app/Http/Controllers/ShowroomsController.php
+++ app/Http/Controllers/ShowroomsController.php
@@ -32,4 +32,28 @@ class ShowroomsController extends Controller
 
         return view('showrooms.index', compact('showrooms', 'places', '_country', '_city'));
     }
+
+    public function show($showroom)
+    {
+        $client = new Client();
+        $res = $client->request('GET', 'http://88.99.171.85/showrooms/');
+        $showrooms = json_decode((string)$res->getBody(), true);
+        $places = [];
+
+        foreach ($showrooms as $item) {
+            if(!isset($places[$item['COUNTRY']]) || !is_array($places[$item['COUNTRY']]))
+                $places[$item['COUNTRY']] = [];
+
+            if(!in_array($item['CITY'], $places[$item['COUNTRY']]))
+                $places[$item['COUNTRY']][] = $item['CITY'];
+        }
+        
+        $showrooms = array_filter($showrooms, function($item) use ($showroom) {
+            if($item['ID'] != $showroom)
+                return false;
+            return true;
+        });
+
+        return view('showrooms.show', compact('showrooms', 'places'));
+    }
 }
--- app/Http/Controllers/StocksController.php
+++ app/Http/Controllers/StocksController.php
@@ -4,17 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Stock;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class StocksController extends Controller
 {
     public function index(Request $request)
     {
+        $months = [];
+        for ($i=0; $i < 11; $i++) {
+            $time = Carbon::now()->addMonth(-$i);
+            $months[$time->formatLocalized('%B %Y')] = [$time->year, $time->month];
+        }
         $stocks = Stock::orderBy('date', 'desc');
         if($request->has('month')) {
-            $stocks = $stocks->whereRaw('MONTH(`date`) = '.$request->month);
+            $stocks = $stocks->whereRaw('YEAR(`date`) = '.$request->year.' AND MONTH(`date`) = '.$request->month);
         }
         $stocks = $stocks->paginate(5);       
-        return view('stocks.index', compact('stocks'));
+        return view('stocks.index', compact('stocks', 'months'));
     }
 
     public function show(Request $request, Stock $stock)
