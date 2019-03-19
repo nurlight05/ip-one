d9298c69e1b293e107c6bd87b1ee1e1c47b8db2e
Max Lee <godmanshot@gmail.com>
2019-03-19 12:09:01 +0600

    1

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -7,11 +7,37 @@
 <section class="container-fluid content inner_pages">
 		<div class="row">
 			<div class="col-md-2 left-side py-5">
-				<h3>Интернет магазин</h3>
+				<h3>Продукты</h3>
 				<h4>Наши продукты</h4>
 				@foreach ($products as $item)
 					<div class="month pl-4"><a href="{{route('products.show', $item)}}">{{$item->name}}</a></div>
 				@endforeach
+				<div class="converter" id="converter">
+					<h6>конвертер валют</h6>
+					<form action="">
+						<div class="convert_box">
+							<p class="part">y.e.</p>
+							<input type="text" value="1" id="num_of_bill">
+						</div>
+						<div class="convert_box">
+							<div class="select_box">
+								<select name="" id="ex_rate">
+									<option value="300">KZT</option>
+									<option value="55">RUB</option>
+									<option value="1">USD</option>
+									<option value="25">UAH</option>
+									<option value="0.7">EUR</option>
+								</select>
+							</div>
+							<input type="text" id="sum_of_bill" value="300">
+						</div>
+					</form>
+					<p>1 y.e. = 300 KZT (тенге)</p>
+					<p>1 y.e. = 55 RUB (рублей)</p>
+					<p>1 y.e. = 1 USD (долларов США)</p>
+					<p>1 y.e. = 25 UAH (гривен)</p>
+					<p>1 y.e. = 0.7 EUR (евро)</p>	
+				</div>
 				<a href="#header_menu" class="toUp_btn"></a>
 			</div>
 			<div class="col-md-10 prod_unit">
