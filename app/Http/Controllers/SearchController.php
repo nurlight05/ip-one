a65f2d206ae0901c5b80490c3683e8b6362f8baf
Max Lee <godmanshot@gmail.com>
2019-03-20 14:11:19 +0600

    1

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\News;
+use App\Stock;
+use App\Content;
+use App\Product;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $products = Product::where('name', 'like', '%'.$request->search.'%')
+                            ->orWhere('mini_description', 'like', '%'.$request->search.'%')
+                            ->orWhere('description', 'like', '%'.$request->search.'%')
+                            ->get();
+
+        $news = News::where('title', 'like', '%'.$request->search.'%')
+                    ->orWhere('mini_description', 'like', '%'.$request->search.'%')
+                    ->orWhere('text', 'like', '%'.$request->search.'%')
+                    ->get();
+
+        $stocks = Stock::where('title', 'like', '%'.$request->search.'%')
+                    ->orWhere('mini_description', 'like', '%'.$request->search.'%')
+                    ->orWhere('text', 'like', '%'.$request->search.'%')
+                    ->get();
+
+
+        $contents = Content::where('name', 'like', '%'.$request->search.'%')
+                        ->orWhere('content', 'like', '%'.$request->search.'%')
+                        ->get();
+
+        return view('search.index', compact('products', 'news', 'stocks', 'contents'));
+    }
 }
