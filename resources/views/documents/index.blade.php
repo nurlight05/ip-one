7cdd25136801a4ae1169ad4628be78112698e73e
Max Lee <godmanshot@gmail.com>
2019-05-27 13:42:50 +0600

    1

--- resources/views/documents/index.blade.php
+++ resources/views/documents/index.blade.php
@@ -11,32 +11,7 @@
         </div>
         <div class="col-md-10 documents p-5">
             <ul id="lightgallery" class="row">
-                <li class="col-md-3" data-src="{{asset('files/Ltut00ExelMFbnb4vFafUWX8DYZu4qy4qEQ5cdNZ.jpeg')}}"><a href><img src="{{asset('files/Ltut00ExelMFbnb4vFafUWX8DYZu4qy4qEQ5cdNZ.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/aK2LHOuvGEps1JvKlCEZPNTTYXql49WSTBylhwio.jpeg')}}"><a href><img src="{{asset('files/aK2LHOuvGEps1JvKlCEZPNTTYXql49WSTBylhwio.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/mCTpDGHxHWxxM5GEpFZbJIP95hvCUooCpBOXtcnm.jpeg')}}"><a href><img src="{{asset('files/mCTpDGHxHWxxM5GEpFZbJIP95hvCUooCpBOXtcnm.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/8EdroOtt5fUn01IaqEt543ERbVuyXQumzRrVtL3o.jpeg')}}"><a href><img src="{{asset('files/8EdroOtt5fUn01IaqEt543ERbVuyXQumzRrVtL3o.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/ZmSrzMl73UkKx9bkKzivTMPtXpxeXRv9m0lfC7AR.jpeg')}}"><a href><img src="{{asset('files/ZmSrzMl73UkKx9bkKzivTMPtXpxeXRv9m0lfC7AR.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/gh27G4FcWo2jD069Sq0VdOIN1SJZcmm4vdCo3xtt.jpeg')}}"><a href><img src="{{asset('files/gh27G4FcWo2jD069Sq0VdOIN1SJZcmm4vdCo3xtt.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/OjwKgbAaVN21kPtt12BzyWNqR7OGEeJHTvr9QLcC.jpeg')}}"><a href><img src="{{asset('files/OjwKgbAaVN21kPtt12BzyWNqR7OGEeJHTvr9QLcC.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/AJiE0mIQnfe1ePqgG1lZO6tl2fYKdr8Hf3CxtD7g.jpeg')}}"><a href><img src="{{asset('files/AJiE0mIQnfe1ePqgG1lZO6tl2fYKdr8Hf3CxtD7g.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/mzOhw0J8E6Kc9uxk8Xxd8dQGUoL6jINmtdV1SWCX.jpeg')}}"><a href><img src="{{asset('files/mzOhw0J8E6Kc9uxk8Xxd8dQGUoL6jINmtdV1SWCX.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/AByWjXM7G4RCN0uXB5QpiFj6IO4KtLtcKnBiuLhR.jpeg')}}"><a href><img src="{{asset('files/AByWjXM7G4RCN0uXB5QpiFj6IO4KtLtcKnBiuLhR.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/rtvSsvMD6hVlYVsLrHIPVSBQ8T2VoHyNom8zi1B6.jpeg')}}"><a href><img src="{{asset('files/rtvSsvMD6hVlYVsLrHIPVSBQ8T2VoHyNom8zi1B6.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/AQkuPTRlxj4FHoPhx8lGSQ3BkyDqCM04s9qULEm0.jpeg')}}"><a href><img src="{{asset('files/AQkuPTRlxj4FHoPhx8lGSQ3BkyDqCM04s9qULEm0.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/iEL0lS5hiHgLzNwnMqF6J11ePTJk2LVszmz8O1eq.jpeg')}}"><a href><img src="{{asset('files/iEL0lS5hiHgLzNwnMqF6J11ePTJk2LVszmz8O1eq.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/7d8VS7QiWGGgGIyzqt3gj6GIswHatYxkaSBuLTf4.png')}}"><a href><img src="{{asset('files/7d8VS7QiWGGgGIyzqt3gj6GIswHatYxkaSBuLTf4.png')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/UioNtap498UAEvlYQUX7ArOdvyezScyQRCy02R4U.jpeg')}}"><a href><img src="{{asset('files/UioNtap498UAEvlYQUX7ArOdvyezScyQRCy02R4U.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/sfWASVRHLeOltNXBk2t1JGoIimZNTGzpESu4ZnZp.jpeg')}}"><a href><img src="{{asset('files/sfWASVRHLeOltNXBk2t1JGoIimZNTGzpESu4ZnZp.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/Py923WpLkGEjM8G3tCE8MPZNC0dkMMVhe42KsQas.jpeg')}}"><a href><img src="{{asset('files/Py923WpLkGEjM8G3tCE8MPZNC0dkMMVhe42KsQas.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/gdPmRyHuyB4mMyQpvSBl3UJIJgnTKpzUV3HiuDOz.jpeg')}}"><a href><img src="{{asset('files/gdPmRyHuyB4mMyQpvSBl3UJIJgnTKpzUV3HiuDOz.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/it6g38gcVabN8RSeCMBtUUcOXAn5szo4m9x8Sdpv.jpeg')}}"><a href><img src="{{asset('files/it6g38gcVabN8RSeCMBtUUcOXAn5szo4m9x8Sdpv.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/uRcTX00nHj0L9pCeAZN9cV4ghUBFvt2qMbIMqj2X.jpeg')}}"><a href><img src="{{asset('files/uRcTX00nHj0L9pCeAZN9cV4ghUBFvt2qMbIMqj2X.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/WfuUzTeBUISJ4PIKzlRukqF2eIXoJhxfCJsMO7jK.png')}}"><a href><img src="{{asset('files/WfuUzTeBUISJ4PIKzlRukqF2eIXoJhxfCJsMO7jK.png')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/XTL67eVzZKXTeJJHuBtIuPsfvuLAMIqFBVd82aF2.png')}}"><a href><img src="{{asset('files/XTL67eVzZKXTeJJHuBtIuPsfvuLAMIqFBVd82aF2.png')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/V937WTaN4Eb7JbTnhFfBPLKHjTRIbtiFsJmkY4WK.png')}}"><a href><img src="{{asset('files/V937WTaN4Eb7JbTnhFfBPLKHjTRIbtiFsJmkY4WK.png')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/ZVw2byHuPhoEg8G0ztJhJMPPBOwSpeRX5hCTiTjg.jpeg')}}"><a href><img src="{{asset('files/ZVw2byHuPhoEg8G0ztJhJMPPBOwSpeRX5hCTiTjg.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/2Xfu4vqaHHKhrFtVMk6d07VI5tc1YJR3U1Asq5rw.jpeg')}}"><a href><img src="{{asset('files/2Xfu4vqaHHKhrFtVMk6d07VI5tc1YJR3U1Asq5rw.jpeg')}}" alt=""></a></li>
-                <li class="col-md-3" data-src="{{asset('files/5o2bZtGDuk50GCoKKdkpjtJS2rmgzLmB255yIG0s.jpeg')}}"><a href><img src="{{asset('files/5o2bZtGDuk50GCoKKdkpjtJS2rmgzLmB255yIG0s.jpeg')}}" alt=""></a></li>
+                {!!$content->content!!}
             </ul>
         </div>
     </div>
