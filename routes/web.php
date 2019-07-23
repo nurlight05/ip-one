5f3a80d279618318e78a870774d33884cc03b96d
Max Lee <godmanshot@gmail.com>
2019-07-23 11:10:03 +0600

    1

--- routes/web.php
+++ routes/web.php
@@ -13,51 +13,50 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::domain('{subdomain?}.ip-one.net')->group(function () {
+Route::domain('{subdomain}.ip-one.net')->group(function () {
     Route::get('/', function (Request $request) {
         dd($request->subdomain);
     });
-
-
-    Route::get('/lang/{locale}', 'HomeController@lang');
-
-    // Route::get('/', 'HomeController@index');
-
-    Route::resource('/news', 'NewsController');
-
-    Route::resource('/stocks', 'StocksController');
-
-    Route::resource('/products', 'ProductsController');
-
-    Route::resource('/bussiness', 'BussinessController');
-
-    Route::get('/showrooms', 'ShowroomsController@index');
-    Route::get('/showrooms/{showroom}', 'ShowroomsController@show');
-
-    Route::get('/science', 'ScienceController@index');
-
-    Route::get('/reviews', 'ReviewsController@index');
-
-    Route::get('/faq', 'FaqController@index');
-
-    Route::get('/info', 'InfoController@index');
-
-    Route::get('/how-to-buy', 'HowToBuyController@index');
-
-    Route::get('/contacts', 'ContactsController@index');
-
-    Route::get('/idea', 'IdeaController@index');
-
-    Route::get('/documents', 'DocumentsController@index');
-
-    Route::get('/video', 'VideoController@index');
-
-    Route::get('/about', 'AboutController@index');
-
-    Route::get('/basket', 'BasketController@index');
-
-    Route::get('/search', 'SearchController@index');
 });
+
+Route::get('/lang/{locale}', 'HomeController@lang');
+
+Route::get('/', 'HomeController@index');
+
+Route::resource('/news', 'NewsController');
+
+Route::resource('/stocks', 'StocksController');
+
+Route::resource('/products', 'ProductsController');
+
+Route::resource('/bussiness', 'BussinessController');
+
+Route::get('/showrooms', 'ShowroomsController@index');
+Route::get('/showrooms/{showroom}', 'ShowroomsController@show');
+
+Route::get('/science', 'ScienceController@index');
+
+Route::get('/reviews', 'ReviewsController@index');
+
+Route::get('/faq', 'FaqController@index');
+
+Route::get('/info', 'InfoController@index');
+
+Route::get('/how-to-buy', 'HowToBuyController@index');
+
+Route::get('/contacts', 'ContactsController@index');
+
+Route::get('/idea', 'IdeaController@index');
+
+Route::get('/documents', 'DocumentsController@index');
+
+Route::get('/video', 'VideoController@index');
+
+Route::get('/about', 'AboutController@index');
+
+Route::get('/basket', 'BasketController@index');
+
+Route::get('/search', 'SearchController@index');
 
 // require __DIR__.'/contents.php';
 
